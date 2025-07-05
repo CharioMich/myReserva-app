a0af750e013900c7147eb00361a55735b85c3647
Ch. Michalakis <mpampisb@gmail.com>
2025-07-06 02:14:41 +0300

    feat: added getCurrentUser api call

---
+++ src/api/users.ts
@@ -0,0 +1,32 @@
+import axios from "./axios.ts";
+import {AxiosError} from "axios";
+import {getCookie} from "../utils/cookie.ts";
+
+const USERS_URL = import.meta.env.VITE_API_URL + "/users";
+
+export const getCurrentUser = async () => {
+
+  const accessToken = getCookie("access_token");
+
+  try {
+    const res = await axios.get(
+      USERS_URL + '/current',
+      {
+        withCredentials: true,
+        headers: {
+          Authorization: `Bearer ${accessToken}`,
+        }
+      });
+    return res.data;
+  } catch (error) {
+    if (error instanceof AxiosError) {
+      if (error.response?.status === 401) {
+        console.error('Login Required');
+      } else {
+        console.error('Axios error:', error.message);
+      }
+    } else {
+      console.error('Unexpected error in Login:', error);
+    }
+  }
+}
