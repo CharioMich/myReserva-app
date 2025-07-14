943a8395c46bd52c0f2f328d114fa5fe50565b42
Ch. Michalakis <mpampisb@gmail.com>
2025-07-15 01:50:27 +0300

    docs(newReservation): added comment spacing

--- src/api/reservations.ts
+++ src/api/reservations.ts
@@ -8,9 +8,11 @@ const RESERVATIONS_URL = import.meta.env.VITE_API_URL + "/reservations";
 
 /**
  * Sends a new reservation request to the backend API.
+ *
  * @param axiosPrivate is an axios instance, enhanced with interceptors (token refresh) using the useAxiosPrivate() hook.
  * React hooks can only be called inside functional components or other hooks so we keep the api logic in this file for separation of concerns
  * and inject the instance as a parameter. (injecting it from useEffect hook in ReservationForm.tsx component)
+ *
  * @param payload The reservation data (date, time, text?)
  */
 export const newReservation = async (axiosPrivate: AxiosInstance, payload: ReservationProps) => {
