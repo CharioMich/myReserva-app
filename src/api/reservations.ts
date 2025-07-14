124490be3185828dcce4a7e2ba83eb84b469292d
Ch. Michalakis <mpampisb@gmail.com>
2025-07-15 01:17:13 +0300

    docs(newReservation): added documentation comments

--- src/api/reservations.ts
+++ src/api/reservations.ts
@@ -6,6 +6,13 @@ import {toast} from "sonner";
 
 const RESERVATIONS_URL = import.meta.env.VITE_API_URL + "/reservations";
 
+/**
+ * Sends a new reservation request to the backend API.
+ * @param axiosPrivate is an axios instance, enhanced with interceptors (token refresh) using the useAxiosPrivate() hook.
+ * React hooks can only be called inside functional components or other hooks so we keep the api logic in this file for separation of concerns
+ * and inject the instance as a parameter. (injecting it from useEffect hook in ReservationForm.tsx component)
+ * @param payload The reservation data (date, time, text?)
+ */
 export const newReservation = async (axiosPrivate: AxiosInstance, payload: ReservationProps) => {
 
   const accessToken = getCookie("access_token");
